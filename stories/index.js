6ebb3fd9d8f66f7e3d34e5251a8f688fcfea0a85
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-09-05 21:47:57 -0400

    refactored the interviewList props and the respective story

--- stories/index.js
+++ stories/index.js
@@ -136,8 +136,8 @@ storiesOf("InterviewerList", module)
   .add("Preselected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
-      setInterviewer={action("setInterviewer")}
+      value={3}
+      onChange={action("setInterviewer")}
     />
   ));
 
