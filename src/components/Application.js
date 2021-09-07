65b7a1d6ef662900f6d94b628156913c5cf2114c
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-09-07 16:37:08 -0400

    took out hard code data and replace with a call to api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 
 import DayList from 'components/DayList';
 import Appointment from "components/Appointment/index";
 
-const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
 
 const appointments = [
     {
@@ -72,17 +56,27 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
 
   const allAppointments = appointments.map(appointment => {
     return(
       <Appointment
+        key={appointment.id}
         id={appointment.id}
         time={appointment.time}
         interview={appointment.interview}
       />
     )
   })
+
+  useEffect(() => {
+    axios.get("/api/days").then(response => {
+      console.log(response.data);
+      setDays([...response.data])
+    })
+
+  },[])
 
   return (
     <main className="layout">
