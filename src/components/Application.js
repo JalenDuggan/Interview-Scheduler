ae478ff68ab61c79bc508e3acaab333bd211b603
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-09-07 21:09:40 -0400

    selector takes the state ovject and finds appointments for the day

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,6 +56,13 @@ const appointments = [
 
 export default function Application(props) {
 
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+
+    appointments: {}
+  });
+
   const [day, setDay] = useState('Monday');
   const [days, setDays] = useState([]);
 
@@ -90,8 +97,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  //returns an array of appointments for that day
+  
+  const result = [];
+  if (day) {
+    for (const dayItem of state.days) {
+      if (dayItem.name === day) {
+        
+        for (const appointmentNum of dayItem.appointments) {
+          result.push(state.appointments[appointmentNum])
+        }
+        return result;
+      }
+    }
+    return result;
+  }
+  return result;
+}
